02a52a200284587a218fed5eb7976595ef88d934
Py-Ru <ashvinmaruti@gmail.com>
2024-04-04 08:39:21 -0700

    second significant commit, task list implemented.

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-tasks = ['foo', 'bar', 'baz']
+tasks = []
 # Create your views here.
 def index(request):
-    return render(request, 'tasks\index.html', {'tasks':tasks})
+    return render(request, 'tasks/index.html', {'tasks':tasks})
+
+def add(request):
+    if request.method == 'POST':
+        task = request.POST.get('task', None)
+        if task is not None:
+            tasks.append(task)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, 'tasks/add.html', {'task': task})
+    return render(request, 'tasks/add.html')
