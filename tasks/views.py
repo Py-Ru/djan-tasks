dfa11d356098d1ca46fa95421a538fc687711963
Py-Ru <ashvinmaruti@gmail.com>
2024-04-05 15:43:41 +0100

    storage to db instead of list

--- tasks/views.py
+++ tasks/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
-tasks = []
+from .models import Task
+
+
 # Create your views here.
 def index(request):
-    return render(request, 'tasks/index.html', {'tasks':tasks})
+    tasks = Task.objects.all()
+    return render(request, 'tasks/index.html', {'tasks': tasks})
 
 def add(request):
     if request.method == 'POST':
         task = request.POST.get('task', None)
         if task is not None:
-            tasks.append(task)
-            return HttpResponseRedirect(reverse('index'))
-        else:
-            return render(request, 'tasks/add.html', {'task': task})
+            new_task = Task.objects.create(task=task)
+            return redirect(reverse('index'))
     return render(request, 'tasks/add.html')
